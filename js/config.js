3f56b6c1bd152623c1543a664a1e36480a248e23
Falgit1 <140887788+Falgit1@users.noreply.github.com>
2025-08-31 16:15:09 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '__USER_POOL_ID__', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '__CLIENT_ID__', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '__REGION__' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '__URL__' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
